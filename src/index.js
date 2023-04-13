9eba15572447e9861a53cb982fe04d8cd7293a28
Aashish Singh <120374078+aashishthakurcodes@users.noreply.github.com>
2023-04-13 06:53:45 +0000

    Database connected

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import mongoose from "mongoose";
+import app from "./app.js";
 
-(async()=>{
+(async () => {
     try {
-        
-    } catch (err) {
-        console.error(err)
-        
+        await mongoose.connect("mongodb://localhost:27017/ecomm");
+        console.log("Database connected");
+
+        app.on('error',(err)=>{
+            console.error("Error:",err);
+            throw err;
+        })
+        const onlistening = () => {
+            console.log(`listening on port 500`);
+        }
+        app.listen(5000, onlistening)
     }
-})()
+
+
+    catch (err) {
+        console.error("Error aa gyi bro", err)
+        throw err;
+    }
+})
+ ()
