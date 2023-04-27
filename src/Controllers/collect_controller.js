3feba14ca2ac945e3baa327d387d5cc7638e1f82
Aashish Singh <120374078+aashishthakurcodes@users.noreply.github.com>
2023-04-27 00:32:03 +0000

    Collection controller

--- src/Controllers/collect_controller.js
+++ src/Controllers/collect_controller.js
@@ -1,6 +1,7 @@
 import collectionschema from "/workspaces/FSJS_Backend/src/Models/collection.js"
 import asynHandler from "../Services/asynchandler.js"
 import customerrors from "../utilis/customerrors.js";
+import collection from "/workspaces/FSJS_Backend/src/Models/collection.js";
 
 
 export const createcollection=asynHandler(async(req,res)=>{
@@ -21,4 +22,72 @@ export const createcollection=asynHandler(async(req,res)=>{
         message: "Collection was created successfully",
         collection
     })
+
 })
+//Update the collectionj
+    export const updateCollection = asynHandler(async(req, res) => {
+        const {name} = req.body
+        const {id: collectionId} = req.params
+    
+    
+        if (!name) {
+            throw new customerrors("Colection name is required", 400)
+        }
+    
+        let updatedCollection = await collection.findByIdAndUpdate(collectionId, {
+            name
+        }, {
+            new: true,
+            runValidators: true
+        })
+    
+        if (!updatedCollection) {
+            throw new customerrors("Colection not found", 400)
+        }
+    
+        res.status(200).json({
+            success: true,
+            message: "Collection updated successfully",
+            updatedCollection
+        })
+    })
+//Delete collection
+    export const deleteCollection = asynHandler(async(req, res) => {
+
+        const {id: collectionId} = req.params
+    
+        const collectionToDelete = await collection.findById(collectionId)
+    
+        if (!collectionToDelete) {
+            throw new customerrors("Colection to be deleted not found", 400)
+        }
+    
+        await collectionToDelete.remove()
+    
+        res.status(200).json({
+            success: true,
+            message: "Collection deleted successfully",
+    
+        })
+    })
+    
+
+    //Get All Collection
+
+    export const getAllCollection = asynHandler(async(req, res) => {
+
+
+
+        const collections = await collection.find()
+    
+        if (!collections) {
+            throw new customerrors("No collection found", 400)
+        }
+    
+        res.status(200).json({
+            success: true,
+            collections
+    
+        })
+    })
+    
